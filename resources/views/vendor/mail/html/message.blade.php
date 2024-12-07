31cb84df2dcc5872be2674db9c6239a0a3231eba
Anon Dev <michaelerastus9@gmail.com>
2024-12-07 10:34:15 +0100

    Added logo

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -3,7 +3,7 @@
 @slot('header')
 @component('mail::header', ['url' => config('app.url')])
 {{--{{ config('app.name') }}--}}
-<img src="{{asset('home/images/aptiscapechain.png')}}" class="logo" alt="{{config('app.name')}}" style="width:100px;">
+<img src="{{asset('home/images/advantaexperts.png')}}" class="logo" alt="{{config('app.name')}}" style="width:100px;">
 @endcomponent
 @endslot
 
